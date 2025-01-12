331d9da3b996585270fda9afc95eaf78a74fa98c
DawidGebert <dawid.gebert@gmail.com>
2025-01-12 14:49:40 +0100

    adding better sorting

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -5,18 +5,33 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FunnyWebRazor.Pages.Categories
 {
+    [BindProperties]
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _db;
         public List<Category> CategoryList { get; set; }
-
+        public String SortType { get; set; }
         public IndexModel(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        public void OnGet()
+        public void OnGet(String sort)
         {
+            Console.WriteLine("this is sort: "+ sort);
+            SortType = "ratingasc";
+            if (sort == "nameasc"){
+                SortType = "nameasc";
+            }
+            if (sort == "ratingasc") {
+                SortType = "ratingasc";
+            }
+            if (sort == "namedesc") {
+                SortType = "namedesc";
+            }
+            if (sort == "ratingdesc") {
+                SortType = "ratingdesc";
+            }
             CategoryList = _db.Categories.ToList();
         }
     }
